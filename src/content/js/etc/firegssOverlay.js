775ff44b8792389ff9420b47f284a9be6b0b88b0
Panagiotis Astithas <pastith@gmail.com>
2009-08-17 18:46:45 +0300

    Add an unfinished implementation of the user login process.

--- src/content/js/etc/firegssOverlay.js
+++ src/content/js/etc/firegssOverlay.js
@@ -13,14 +13,35 @@ function loadFireGSS() {
   } catch (ex) {
     loadInTab = true;
   }
-
+  
   if (loadInTab) {
     var theTab          = gBrowser.addTab('chrome://firegss/content/');
     theTab.label        = "FireGSS";
     gBrowser.selectedTab = theTab;
     var func = function () { gBrowser.setIcon(theTab, "chrome://firegss/skin/icons/logo.png"); };
     setTimeout(func, 500);
+
+    var nonce_callback = function (data, textStatus) {
+      gss.nonce = data;
+      var theTab          = gBrowser.addTab(gss.LOGIN_URL+'?nonce='+data);
+      theTab.label        = "Login";
+      gBrowser.selectedTab = theTab;
+      // on tab close do:
+      setTimeout(getToken, 10000);
+    };
+  
+    jQuery.get(gss.NONCE_URL, {"user": gss.username}, nonce_callback, "text");
   } else {
     toOpenWindowByType('mozilla:firegss', 'chrome://firegss/content/');
   }
+}
+
+function getToken() {
+  jQuery.get(gss.TOKEN_URL, {"user": gss.username, "nonce": gss.nonce}, token_callback, "text");
+}
+
+function token_callback(data, textStatus) {
+  var cons = Components.classes["@mozilla.org/consoleservice;1"].
+    	getService(Components.interfaces.nsIConsoleService);
+  cons.logStringMessage("Got token: "+data);
 }

40c29cdfc6ca950b373098b9192e9208a520b1aa
Panagiotis Astithas <pastith@gmail.com>
2009-08-18 18:19:10 +0300

    Fix token retrieval and automatic page redirection.

--- src/content/js/etc/firegssOverlay.js
+++ src/content/js/etc/firegssOverlay.js
@@ -4,44 +4,68 @@
  */
 
 function loadFireGSS() {
-  var prefService    = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-  var prefSvc        = prefService.getBranch(null);
+  var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+      .getService(Components.interfaces.nsIPrefService).getBranch(null);
+  var cons = Components.classes["@mozilla.org/consoleservice;1"].
+      getService(Components.interfaces.nsIConsoleService);
 
-  var loadInTab      = false;
+  var loadInTab = false;
   try {
-    loadInTab        = prefSvc.getIntPref("firegss.loadmode");
+    loadInTab = prefs.getIntPref("firegss.loadmode");
   } catch (ex) {
     loadInTab = true;
   }
   
-  if (loadInTab) {
-    var theTab          = gBrowser.addTab('chrome://firegss/content/');
-    theTab.label        = "FireGSS";
+  var showLogin = function (data) {
+    gss.nonce = data;
+    //if (loadInTab) {
+    var theTab = gBrowser.addTab(gss.LOGIN_URL+'?nonce='+data);
+    theTab.label = "Login";
     gBrowser.selectedTab = theTab;
-    var func = function () { gBrowser.setIcon(theTab, "chrome://firegss/skin/icons/logo.png"); };
-    setTimeout(func, 500);
+    var newTabBrowser = gBrowser.getBrowserForTab(theTab);
+    newTabBrowser.addEventListener("load", function() {
+      var index;
+      if ((index = newTabBrowser.contentDocument.body.innerHTML.indexOf("You can now close")) !== -1) {
+        /*var openPage = function (data) {
+          cons.logStringMessage("Got token: "+data);
+          var newTabBrowser = gBrowser.getBrowserForTab(theTab);
+          alert(newTabBrowser.contentDocument.body.innerHTML);
+          var theTab = gBrowser.addTab('chrome://firegss/content/');
+          theTab.label = "FireGSS";
+          gBrowser.selectedTab = theTab;
+          var func = function () { gBrowser.setIcon(theTab, "chrome://firegss/skin/icons/logo.png"); };
+          setTimeout(func, 500);
+        };
+        cons.logStringMessage("jQuery:"+jQuery);
+        jQuery.get(gss.TOKEN_URL, {"user": gss.username, "nonce": gss.nonce}, openPage, "text");*/
+   	    var req = new XMLHttpRequest();
+	    req.open('GET', gss.TOKEN_URL+'?user='+gss.username+'&nonce='+gss.nonce, true);
+	    req.onreadystatechange = function (aEvt) {
+	      if (req.readyState == 4) {
+            if(req.status == 200) {
+              gss.authToken = data;
+              var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                   .getService(Components.interfaces.nsIWindowMediator);
+              var mainWindow = wm.getMostRecentWindow("navigator:browser");
+              var gBrowser = mainWindow.getBrowser();
+              gBrowser.removeCurrentTab();
+              var theTab = gBrowser.addTab('chrome://firegss/content/');
+              theTab.label = "FireGSS";
+              gBrowser.selectedTab = theTab;
+              var func = function () { gBrowser.setIcon(theTab, "chrome://firegss/skin/icons/logo.png"); };
+              setTimeout(func, 500);
+            } else
+              cons.logStringMessage("Error getting token. req.status="+req.status);
+	      }
+	    };
+	    req.send(null);
 
-    var nonce_callback = function (data, textStatus) {
-      gss.nonce = data;
-      var theTab          = gBrowser.addTab(gss.LOGIN_URL+'?nonce='+data);
-      theTab.label        = "Login";
-      gBrowser.selectedTab = theTab;
-      // on tab close do:
-      setTimeout(getToken, 10000);
-    };
-  
-    jQuery.get(gss.NONCE_URL, {"user": gss.username}, nonce_callback, "text");
-  } else {
-    toOpenWindowByType('mozilla:firegss', 'chrome://firegss/content/');
-  }
-}
+      }
+    }, true);
+    //} else {
+      //toOpenWindowByType('mozilla:firegss', 'chrome://firegss/content/');
+    //}
+  };
 
-function getToken() {
-  jQuery.get(gss.TOKEN_URL, {"user": gss.username, "nonce": gss.nonce}, token_callback, "text");
-}
-
-function token_callback(data, textStatus) {
-  var cons = Components.classes["@mozilla.org/consoleservice;1"].
-    	getService(Components.interfaces.nsIConsoleService);
-  cons.logStringMessage("Got token: "+data);
+  jQuery.get(gss.NONCE_URL, {"user": gss.username}, showLogin, "text");
 }

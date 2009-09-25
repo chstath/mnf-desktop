d473bc872c6d15cf6fd4f6880fb118c0cbafb062
Panagiotis Astithas <pastith@gmail.com>
2009-09-25 18:50:03 +0300

    Open the sync dialog after the initial sync folder preference configuration.

--- src/content/js/etc/preferences.js
+++ src/content/js/etc/preferences.js
@@ -79,11 +79,11 @@ function readPreferences(startup) {
   }
 }
 
-function showPreferences(tab) {
+function showPreferences(args) {
   var branch = gPrefsService.getBranch("browser.");
   var instantApply = branch.getBoolPref("preferences.instantApply");
   window.openDialog("chrome://firegss/content/preferences.xul", "preferences",
-        "chrome,resizable,centerscreen" + (instantApply ? ",dialog=no" : ",modal,dialog"), tab);
+        "chrome,resizable,centerscreen" + (instantApply ? ",dialog=no" : ",modal,dialog"), args);
 }
 
 var prefsObserver = {
--- src/content/js/etc/sync.js
+++ src/content/js/etc/sync.js
@@ -1,6 +1,24 @@
 function sync() {
     if (gSyncFolder === "") {
-        showPreferences(2);
-    }
+        showPreferences({ tab: 2, next: showSync });
+    } else
+        showSync();
 }
 
+function showSync() {
+    window.openDialog("chrome://firegss/content/sync.xul", "sync",
+            "chrome,modal,dialog,centerscreen");
+
+}
+
+function init() {
+    
+}
+
+function syncUp() {
+
+}
+
+function syncDown() {
+
+}

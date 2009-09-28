de1dc4cc61ae125e065839a4330ae8e8173828f3
Panagiotis Astithas <pastith@gmail.com>
2009-09-28 17:00:24 +0300

    Initial work on sync backend.

---
+++ src/content/js/etc/loadUnload.js
@@ -0,0 +1,250 @@
+function startup() {
+  if (gStrbundle) {                            // we get two onload events b/c of the embedded browser
+    return;
+  }
+
+  window.onerror         = detailedError;
+  gStrbundle             = $("strings");
+  gConnectButton         = $('connectbutton');
+  gAccountField          = $('account');
+  gFolderField           = $('folder');
+  gLocalPath             = $('localpath');
+  gLocalTree             = $('localtree');
+  gLocalDirTree          = $('localdirtree');
+  gLocalTreeChildren     = $('localtreechildren');
+  gLocalDirTreeChildren  = $('localdirtreechildren');
+  gRemotePath            = $('remotepath');
+  gRemoteTree            = $('remotetree');
+  gRemoteDirTree         = $('remotedirtree');
+  gRemoteTreeChildren    = $('remotetreechildren');
+  gRemoteDirTreeChildren = $('remotedirtreechildren');
+  gCmdlogDoc             = $('cmdlog').contentWindow.document;
+  gCmdlogBody            = $('cmdlog').contentWindow.document.body;
+  gQueueTree             = $('queuetree');
+  gQueueTreeChildren     = $('queuetreechildren');
+  gStatusPercent         = $('statuspct');
+  gStatusMeter           = $('statusmeter');
+  gLocalTree.view        = localTree;
+  gLocalDirTree.view     = localDirTree;
+  gRemoteTree.view       = remoteTree;
+  gRemoteDirTree.view    = remoteDirTree;
+  gQueueTree.view        = queueTree;
+
+  gProfileDir            = Components.classes["@mozilla.org/file/directory_service;1"].createInstance(Components.interfaces.nsIProperties)
+                                     .get("ProfD", Components.interfaces.nsILocalFile);
+  gAtomService           = Components.classes["@mozilla.org/atom-service;1"].getService            (Components.interfaces.nsIAtomService);
+  gLoginManager          = Components.classes["@mozilla.org/login-manager;1"].getService           (Components.interfaces.nsILoginManager);
+  gIos                   = Components.classes["@mozilla.org/network/io-service;1"].getService      (Components.interfaces.nsIIOService);
+  gPromptService         = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
+  gPrefsService          = Components.classes["@mozilla.org/preferences-service;1"].getService     (Components.interfaces.nsIPrefService);
+  gFormHistory           = Components.classes["@mozilla.org/satchel/form-history;1"].getService    (Components.interfaces.nsIFormHistory ?
+                                                                                                    Components.interfaces.nsIFormHistory :
+                                                                                                    Components.interfaces.nsIFormHistory2);
+  gLoginInfo             = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1",     Components.interfaces.nsILoginInfo, "init");
+
+  gPrefs                 = gPrefsService.getBranch("firegss.");
+  gPlatform              = getPlatform();
+
+  if (gPrefsService instanceof Components.interfaces.nsIPrefBranchInternal) {
+    gPrefsService.addObserver("firegss", prefsObserver, false);
+  }
+
+  gMaxCon                = gPrefs.getIntPref("concurrentmax");
+
+  gConnections           = new Array();
+  for (var x = 0; x < gMaxCon; ++x) {
+    gConnections.push(new ftpMozilla(x ? new transferObserver(x + 1) : ftpObserver));
+    gConnections[x].type            = x ? 'transfer' : '';
+    gConnections[x].connNo          = x + 1;
+    gConnections[x].errorConnectStr = gStrbundle.getString("errorConn");
+    gConnections[x].errorXCheckFail = gStrbundle.getString("errorXCheckFail");
+    gConnections[x].passNotShown    = gStrbundle.getString("passNotShown");
+    gConnections[x].l10nMonths      = gStrbundle.getString("months").split("|");
+  }
+
+  gFtp                   = gConnections[0];
+  gFtp.setSecurity("");
+
+  gTransferTypes         = new Array(gStrbundle.getString("auto"), gStrbundle.getString("binary"), gStrbundle.getString("ascii"));
+  gMonths                = gStrbundle.getString("months").split("|");
+
+  treeHighlighter.valid  = new Array({ tree: gLocalTree,  children: gLocalTreeChildren,  column: "localname"  },
+                                     { tree: gRemoteTree, children: gRemoteTreeChildren, column: "remotename" },
+                                     { tree: gQueueTree,  children: gQueueTreeChildren });
+
+  if ($('searchWhich').selectedIndex == -1) {
+    $('searchWhich').selectedIndex = 0;
+  }
+
+  searchSelectType();
+  showSearchDates();
+  securityPopup();
+
+  readPreferences(true);
+  localDirTree.changeDir(gLocalPath.value);
+
+  appendLog("<span id='opening' style='line-height:16px'><span style='cursor:pointer;text-decoration:underline;color:blue;' onclick=\"window.open('http://code.google.com/p/gss','FireGSS');\">"
+      + "FireGSS</span> <span>" + gVersion
+      + "</span>"
+      + "</span><br style='font-size:5pt'/><br style='font-size:5pt'/>", 'blue', "info");
+  gCmdlogBody.scrollTop = 0;
+
+  if (gUsernameMode) {
+    var username = gPrefs.getCharPref("username");
+    if (username)
+      jQuery('#username').val(username);
+  }
+  jQuery('#username').formHints({'className':'hint'});
+
+  setTimeout(doResizeHack, 0);
+
+  if (gLoadUrl) {
+    setTimeout("externalLink()", 1000);
+  }
+}
+
+function doDesktopLogin() {
+  var showLogin = function (data) {
+    gss.nonce = data.trim();
+    var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+         .getService(Components.interfaces.nsIWindowMediator);
+    var mainWindow = wm.getMostRecentWindow("navigator:browser");
+    var gBrowser = mainWindow.getBrowser();
+    var theTab = gBrowser.addTab(gss.LOGIN_URL+'?nonce='+gss.nonce);
+    theTab.label = "Login";
+    theTab.setAttribute('gss-login', "xyz");
+    gBrowser.selectedTab = theTab;
+    var newTabBrowser = gBrowser.getBrowserForTab(theTab);
+    newTabBrowser.addEventListener("load", function() {
+      var index;
+      if ((index = newTabBrowser.contentDocument.body.innerHTML.indexOf("You can now close")) !== -1) {
+   	    var req = new XMLHttpRequest();
+	    req.open('GET', gss.TOKEN_URL+'?user='+gss.username+'&nonce='+gss.nonce, true);
+	    req.onreadystatechange = function (aEvt) {
+	      if (req.readyState == 4) {
+            switch (req.status) {
+              case 200:
+                gss.authToken = req.responseText.trim();
+                // Close login tab and return to application tab.
+                var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                     .getService(Components.interfaces.nsIWindowMediator);
+                var mainWindow = wm.getMostRecentWindow("navigator:browser");
+                var gBrowser = mainWindow.getBrowser();
+                var loginTab = returnToAppTab('gss-login');
+                gBrowser.removeTab(loginTab);
+                returnToAppTab('firegss');
+                // Make the username textbox read-only and initialize the remote pane.
+                jQuery("#username").attr("readonly", "true");
+                gss.fetchRootFolder(remoteDirTree.initialize);
+                break;
+              case 403:
+                alert("No matching token was found");
+                break;
+              default:
+                alert("Error getting token. req.status="+req.status);
+            }
+	      }
+	    };
+	    req.send(null);
+      }
+    }, true);
+  };
+  jQuery.ajax({
+    url: gss.NONCE_URL,
+    data: {user: gss.username},
+    success: showLogin,
+    dataType: "text",
+    error: function(request, status, error) {
+      if (request.status === 403)
+        alert("Username not found");
+    }
+  });
+}
+
+function beforeUnload() {
+  return "";
+}
+
+function unload() {
+  if (gPrefsService instanceof Components.interfaces.nsIPrefBranchInternal) {
+    gPrefsService.removeObserver("firegss", prefsObserver, false);
+  }
+
+  for (var x = 0; x < gMaxCon; ++x) {
+    if (gConnections[x].isConnected) {
+      gConnections[x].disconnect();
+    }
+  }
+
+  if (gFxp && gFxp.isConnected) {
+    gFxp.disconnect();
+  }
+
+  for (var x = 0; x < gTempEditFiles.length; ++x) {
+    gfiregssUtils.removeFile(gTempEditFiles[x].file);
+  }
+}
+
+function returnToAppTab(attrName) {
+  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+           .getService(Components.interfaces.nsIWindowMediator);
+  for (var found = false, index = 0, tabbrowser = wm.getEnumerator('navigator:browser').getNext().getBrowser();
+       index < tabbrowser.mTabs.length && !found; index++) {
+
+    // Get the next tab
+    var currentTab = tabbrowser.mTabs[index];
+    // Does this tab contain our custom attribute?
+    if (currentTab.hasAttribute(attrName)) {
+      // Yes--select and focus it.
+      tabbrowser.selectedTab = currentTab;
+      // Focus *this* browser in case another one is currently focused
+      tabbrowser.focus();
+      found = true;
+    }
+  }
+  return currentTab;
+}
+
+function login(event) {
+  event = event.trim();
+  var hint = "Username?";
+  if (event === hint || event === '')
+    alert("Enter a username first");
+  else if (gss.username !== event) {
+    gss.username = event;
+    gss.authToken = '';
+    doDesktopLogin();
+    prefs = Components.classes["@mozilla.org/preferences-service;1"]
+        .getService(Components.interfaces.nsIPrefService)
+        .getBranch("firegss.");
+    prefs.setCharPref("username", gss.username);
+  }
+}
+
+function logout() {
+  if (gss.username === '') return;
+  gss.username = gss.authToken = '';
+  jQuery("#username").val("");
+  jQuery("#username").removeAttr("readonly");
+  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+       .getService(Components.interfaces.nsIWindowMediator);
+  var mainWindow = wm.getMostRecentWindow("navigator:browser");
+  var gBrowser = mainWindow.getBrowser();
+  var theTab = gBrowser.addTab(gss.LOGOUT_URL);
+  theTab.label = "Logout";
+  gBrowser.selectedTab = theTab;
+  // Clear Shibboleth cookies after logout is complete, so that restarting
+  // the browser is unnecessary.
+  var newTabBrowser = gBrowser.getBrowserForTab(theTab);
+    newTabBrowser.addEventListener("load", function() {
+      var cookieMgr = Components.classes["@mozilla.org/cookiemanager;1"]
+                 .getService(Components.interfaces.nsICookieManager);
+      for (var e = cookieMgr.enumerator; e.hasMoreElements();) {
+        var cookie = e.getNext().QueryInterface(Components.interfaces.nsICookie);
+        if (cookie.host === gss.SERVER && (cookie.name.indexOf("_shibstate") == 0
+                                      || cookie.name.indexOf("_shibsession") == 0)) {
+          cookieMgr.remove(cookie.host, cookie.name, cookie.path, false);
+        }
+      }
+    }, true);
+}
---
+++ src/content/js/etc/sync.js
@@ -0,0 +1,85 @@
+var sync;
+if (!sync) sync = {};
+
+sync.open = function () {
+    if (gSyncFolder === "") {
+        showPreferences({ tab: 2, next: sync.showSync });
+    } else
+        sync.showSync();
+}
+
+sync.showSync = function () {
+    window.openDialog("chrome://firegss/content/sync.xul", "sync",
+            "chrome,modal,dialog,centerscreen");
+}
+
+sync.init = function () {
+    
+}
+
+sync.syncUp = function () {
+//    gRemoteSyncFolder
+}
+
+sync.syncDown = function () {
+
+}
+
+// The sync queue of upload requests for files.
+sync.uploadq = [];
+// The sync upload queue monitor used for locking.
+sync.uploading = false;
+// Processes the queue of pending uploads.
+sync.processUploadq = function () {
+    if (sync.uploading) return;
+    var upload = sync.uploadq.shift();
+    if (upload) {
+        sync.uploading = gss.uploadFile(upload.file, upload.folder, upload.onstart,
+	        upload.onprogress, upload.onload, upload.onerror, upload.onabort);
+    } else
+        sync.uploading = false;
+}
+//setInterval(sync.processUploadq, 300);
+// The sync queue of download requests for files.
+sync.downloadq = [];
+// The sync download queue monitor used for locking.
+sync.downloading = false;
+// Processes the queue of pending downloads.
+sync.processDownloadq = function () {
+    if (sync.downloading) return;
+    var download = sync.downloadq.shift();
+    if (download) {
+        try {
+            sync.downloading = download.persist;
+            var now = (new Date()).toUTCString();
+           	// Unfortunately single quotes are not escaped by default.
+            var resource = download.file.uri.replace(/'/g, "%27");
+            var authHeader = "Authorization: " + gss.username + " " +
+                gss.sign('GET', now, resource, gss.authToken);
+            var dateHeader = "X-GSS-Date: " + now;
+            var headers = authHeader + "\r\n" + dateHeader + "\r\n";
+		    var nsIURI = gIos.newURI(resource, "utf-8", null);
+		    var result = download.persist.saveURI(nsIURI, null, null, null, headers, download.nsIFile);
+		    if (result)
+		        alert(result);
+        } catch (e) {
+            alert(e);
+        }
+    } else
+        sync.downloading = false;
+}
+//setInterval(processDownloadq, 300);
+
+sync.cancelAll = function () {
+    sync.downloadq = [];
+    sync.uploadq = [];
+    if (sync.downloading) {
+        sync.downloading.cancelSave();
+        sync.downloading = false;
+    }
+    if (sync.uploading) {
+        sync.uploading.abort();
+        sync.uploading = false;
+    }
+}
+

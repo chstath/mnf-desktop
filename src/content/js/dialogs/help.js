644ebe3ceac1dcdd425894b6e0d60b7a9c94773d
marcello <marcello@marcello-PC>
2009-10-05 14:20:49 +0300

    Add Help Text (Issue 23)

---
+++ src/content/js/dialogs/help.js
@@ -0,0 +1,17 @@
+function init(){
+
+}//init()
+
+//------------------------------------------------------------------------------
+
+function doAccept(){
+    return true;
+}//doOK
+
+//------------------------------------------------------------------------------
+
+function showHelp(){
+            window.openDialog("chrome://firegss/content/help.xul", "help",
+            "chrome,modal,dialog,centerscreen");
+}
+//------------------------------------------------------------------------------

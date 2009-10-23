5b22b52c65ec9cc1dd493e2abf0fd62316cc9e08
Panagiotis Astithas <pastith@gmail.com>
2009-10-23 16:43:29 +0300

    Add a help menu with the main site, issue reporting and about. Also, retrieve the version number from the Extension Manager.

--- src/content/js/dialogs/help.js
+++ src/content/js/dialogs/help.js
@@ -1,19 +1,13 @@
-function init(){
-
-}//init()
-
-//------------------------------------------------------------------------------
-
-function doAccept(){
-    return true;
-}//doAccept
-
-//------------------------------------------------------------------------------
-
-function showHelp(){
+function showHelp() {
 //            window.openDialog("chrome://firegss/content/help.xul", "help",
 //            "chrome,modal,dialog,centerscreen");
-
     runInFirefox("http://code.google.com/p/firegss");
 }
-//------------------------------------------------------------------------------
+
+function showBugs() {
+    runInFirefox("http://code.google.com/p/firegss/issues/list");
+}
+
+function showAbout() {
+    openDialog("chrome://firegss/content/about.xul", "about", "chrome,resizable=yes,centerscreen=yes");
+}

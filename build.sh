274353d329a57ce9c1a29d1c32d356ef151fff1e
Panagiotis Astithas <pastith@gmail.com>
2009-11-17 17:50:19 +0200

    Initial work for the standalone application version of FireGSS. Running the build script is required for properly packaging the code. Launching the app is currently done from the app folder, using firefox -app application.ini.

--- build.sh
+++ build.sh
@@ -6,9 +6,20 @@ NAME=firegss
 XPI=$NAME.xpi
 BUILD_DIR=build
 
+# Build extension.
 mkdir -p $BUILD_DIR
 rm $BUILD_DIR/$XPI >/dev/null 2>&1
 cd src
+cd ..
+
+# Build application.
+cd src
 zip -qDr ../$BUILD_DIR/$XPI .
+mv content/firegss.xul content/main.xul
+zip -qDr ../app/chrome/content.jar content
+mv content/main.xul content/firegss.xul
+cp defaults/preferences/firegss.js ../app/defaults/preferences/
+zip -qDr ../app/chrome/locale.jar locale
+zip -qDr ../app/chrome/skin.jar skin
 cd ..
 

8e3790fbc9eea9e0ef928f358d8c31825f2444a0
Panagiotis Astithas <pastith@gmail.com>
2009-11-26 10:57:43 +0200

    Don't show a warning when viewing the mixed HTTP/HTTPS content, caused by the login procedure.

--- app/defaults/preferences/prefs.js
+++ app/defaults/preferences/prefs.js
@@ -1,6 +1,7 @@
 pref("toolkit.defaultChromeURI", "chrome://firegss/content/main.xul");
 pref("toolkit.defaultChromeFeatures", "chrome,centerscreen,dialog=no,all");
 pref("toolkit.singletonWindowType", "firegssmain");
+pref("security.warn_viewing_mixed", false);
 /* debugging prefs */
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);

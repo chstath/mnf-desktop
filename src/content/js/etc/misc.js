acb4cedaa0081f2dde3afaeca2bd651d0fae0ea7
Panagiotis Astithas <pastith@gmail.com>
2009-11-27 13:06:08 +0200

    Partially revert changeset 2c5a0be5804c, since there is a simpler yet undocumented workaround to install a custom profile.

--- src/content/js/etc/misc.js
+++ src/content/js/etc/misc.js
@@ -221,10 +221,4 @@ function doResizeHack() {
   $('remoteview').setAttribute(      'width', $('remoteview').boxObject.width);
   $('remoteview').removeAttribute(   'flex');
 }
-function isStandalone() {
-  var extManager = Cc["@mozilla.org/extensions/manager;1"].getService(Ci.nsIExtensionManager);
-  var addon = extManager.getItemForID("firegss@ebs.gr"); 
-  if (addon)
-    return false;
-  return true;
-}
+
